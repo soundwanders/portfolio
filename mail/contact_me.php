c7adda9ea969760152be68151ddfead7f05b3f80
soundwanders <soundwanders@gmail.com>
2021-05-16 15:12:01 -0400

    folder index change, portfolio nested within portfolio was messy

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -8,7 +8,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !$email_address)
    {
-	echo "No arguments Provided!";
+	echo "No arguments provided";
 	return false;
    }
 
@@ -22,14 +22,14 @@ $message = $_POST['message'];
 
 if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) don't send!
     // Create the email and send the message
-    $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'soundwanders@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Website Contact Form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-    $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+    $headers = "From: noreply@jcoletta.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
     $headers .= "Reply-To: $email_address";
     mail($to,$email_subject,$email_body,$headers);
     return true;
 }
-echo "Gotcha, spambot!";
+echo "Caught, spambot";
 return false;
 ?>
